3489cc40ea718b915ee1967c69847b2476e259ba
Raj <rajsriv.14@gmail.com>
2022-04-23 15:09:26 +0530

    Added Try Catch block for APIs

--- backend-api+scrapping/controllers/student_controller.js
+++ backend-api+scrapping/controllers/student_controller.js
@@ -1,31 +1,47 @@
 const Student = require("../models/student")
 
 module.exports.getAllStudentsDetail = async function(req,res) {
-    let studentData = await Student.find();
-    return res.status(200).json({
-        data: {
-            studentData: studentData
-        },
-        message: "Student Data Recieved",
-    })
+    try {
+        let studentData = await Student.find();
+        return res.status(200).json({
+            data: {
+                studentData: studentData
+            },
+            message: "Student Data Recieved",
+        })
+    } catch(error) {
+        const errorMessage = `Error on retrieving students : ${err}`;
+		console.log(errorMessage);
+        return res.status(404).json({
+            error: errorMessage
+        })
+    }
 }
 
 module.exports.getStudentDataByRollNo = async function(req,res) {
-    let studentDetail = await Student.findOne({"rollNumber": req.params.id});
-    return res.status(200).json({
-        data: {
-            studentDetail: studentDetail
-        },
+    try{
+        let studentDetail = await Student.findOne({"rollNumber": req.params.id});
+        return res.status(200).json({
+            data: {
+                studentDetail: studentDetail
+            },
         message: "Single Student Detail Recieved",
-    })
+        });
+    } catch(err) {
+        const errorMessage = `Error on retrieving student : ${err}`;
+		console.log(errorMessage);
+        return res.status(404).json({
+            error: errorMessage
+        })
+    }
 }
 
 module.exports.deleteAllStudents = async function(req,res) {
     try{
 		let comment = await Student.deleteMany();
-            return res.status(200).json({
-                message: "All Students deleted"
-            });
+        return res.status(200).json({
+            message: "All Students deleted"
+        });
 	}catch(err){
         const errorMessage = `Error deleting students : ${err}`;
 		console.log(errorMessage);
--- backend-api+scrapping/helpers/mainHelper.js
+++ backend-api+scrapping/helpers/mainHelper.js
@@ -8,25 +8,30 @@ function populateRoll(roll,startRoll,rangeRoll){
 }
 
 exports.saveBranchStudentsinDB = async function (startRoll, rangeRoll, students) {
-    let roll = []
-    populateRoll(roll,startRoll,rangeRoll);
-    let studentData = []
-    studentData = await scrapper.screenShot(roll);
-    let tempStudentData = []
-    for(let i=0;i<studentData.length;i++){
-        let percentNumber = Number(studentData[i]['percent']);
-        if(percentNumber>100 || percentNumber<=10) {
-            continue;
+    try {
+        let roll = []
+        populateRoll(roll,startRoll,rangeRoll);
+        let studentData = []
+        studentData = await scrapper.screenShot(roll);
+        let tempStudentData = []
+        for(let i=0;i<studentData.length;i++){
+            let percentNumber = Number(studentData[i]['percent']);
+            if(percentNumber>100 || percentNumber<=10) {
+                continue;
+            }
+            let rollNo = studentData[i]['rollNumber'];
+            let exists = await Student.findOne({
+                rollNumber: rollNo 
+            });
+            if(!exists){
+                var student = await Student.create(studentData[i])
+                student.save()
+            }
+            tempStudentData.push(studentData[i]);
         }
-        let rollNo = studentData[i]['rollNumber'];
-        let exists = await Student.findOne({
-            rollNumber: rollNo 
-        });
-        if(!exists){
-            var student = await Student.create(studentData[i])
-            student.save()
-        }
-        tempStudentData.push(studentData[i]);
+        students.push(tempStudentData)
+    } catch(error) {
+        const errorMessage = `Error while scrapping/adding data to DB : ${err}`;
+		console.log(errorMessage);
     }
-    students.push(tempStudentData)
 }
