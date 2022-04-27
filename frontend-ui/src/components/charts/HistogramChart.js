ea16bc25591d98a9e4fc7b113fc7db0cba91567b
Tejaswi-19 <63488976+Tejaswi-19@users.noreply.github.com>
2022-04-27 13:05:37 +0530

    Added Charts page
    Added Scatter and Histogram chart

--- frontend-ui/src/components/charts/HistogramChart.js
+++ frontend-ui/src/components/charts/HistogramChart.js
@@ -1,25 +1,5 @@
 import { Chart } from "react-google-charts";
 
-export const data = [
-  ["Student", "Percent"],
-  ["16212", 76],
-  ["16221", 66],
-  ["16238", 86],
-  ["16243", 73],
-  ["16255", 62],
-];
-export const options = {
-  hAxis: {
-    title: "Roll Number",
-  },
-  vAxis: {
-    title: "Percentage",
-  },
-  // series: {
-  //   1: { curveType: "function" },
-  // },
-};
-
 export const data2 = [
   ["Student", "Percent"],
   ["Raj - 16243", 76],
@@ -34,7 +14,7 @@ export const data2 = [
   ["Abhi - 16213", 67],
 ];
 
-export const options2 = {
+const options = {
   title: "Marks Distribution Among Students",
   hAxis: {
     title: "Marks",
@@ -45,28 +25,22 @@ export const options2 = {
   legend: { position: "none" },
   colors: ["orange"],
 };
-
-const Error = () => {
+const HistogramChart = ({ tableData }) => {
+  const data = [["Student", "Percentage"]];
+  tableData.forEach((row) => {
+    data.push([`${row.name} - ${row.rollNumber}`, +row.percent]);
+  });
   return (
     <div>
-      <h1>Error 404</h1>
       <Chart
-        chartType="ScatterChart"
+        chartType="Histogram"
         width="100%"
         height="400px"
         data={data}
         options={options}
       />
-
-      <Chart
-        chartType="Histogram"
-        width="100%"
-        height="400px"
-        data={data2}
-        options={options2}
-      />
     </div>
   );
 };
 
-export default Error;
+export default HistogramChart;
